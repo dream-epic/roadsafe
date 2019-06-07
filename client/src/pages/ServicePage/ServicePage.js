fb592353b8120c9d572cdd4e685fd20338aabc7b
heguanelvis <heguanelvis@gmail.com>
2019-06-06 19:30:35 -0500

    service bot centered

--- client/src/pages/ServicePage/ServicePage.js
+++ client/src/pages/ServicePage/ServicePage.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import ServiceBot from "../../components/ServiceBot";
+import "./ServicePage.scss";
 
 export default class ServicePage extends Component {
   render() {
     return (
-      <div>
+      <div className="text-center">
         <h1>This is the servicepage!</h1>
-        <ServiceBot />
+        <div className="bot-center">
+          <ServiceBot />
+        </div>
       </div>
     );
   }
