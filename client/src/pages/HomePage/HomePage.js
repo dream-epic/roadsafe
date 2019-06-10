5f15d44b0b7277135bb4ea37dd9acb5f767f56bb
Ruolin Fang <ruolin@uchicago.edu>
2019-06-10 14:06:22 -0500

    added cheerio and request

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import cheerio from "cheerio";
+import request from "request";
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>This is the homepage!</h1>
-    </div>
-  );
+export default class HomePage extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    const url = "https://cors-anywhere.herokuapp.com/https://chicago.cbslocal.com/tag/potholes/";
+    this.loadNewsData(url);
+  }
+
+  loadNewsData(url) {
+    console.log(url)
+    request(url, (err, scrapeRes, html) => {
+      const $ = cheerio.load(html);
+      console.log($.html())
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is the homepage!</h1>
+      </div>
+    );
+  }
 }
