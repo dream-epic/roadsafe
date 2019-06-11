5ca01735b524ede80430af476c230e862bef4d47
heguanelvis <heguanelvis@gmail.com>
2019-06-11 13:28:19 -0500

    added jumbotron

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import cheerio from "cheerio";
 import request from "request";
 import News from "../../components/News";
+import Introduction from "../../components/Introduction";
 import "bootstrap-css-only";
+import "./HomePage.scss";
 
 export default class HomePage extends Component {
   state = {
@@ -42,25 +44,26 @@ export default class HomePage extends Component {
 
   render() {
     return (
-      <div>
+      <>
+        <Introduction />
         {this.state.data.length ? (
-          <div className="row justify-content-around">
-          {this.state.data.map(e => (
-            <div className="col-12 col-md-6 col-lg-4">
-            <News
-              key={e.articleLink}
-              title={e.title}
-              imgSrc={e.imgSrc}
-              description={e.description}
-              articleLink={e.articleLink}
-            />
-            </div>
-          ))}
+          <div className="row justify-content-around align-items-stretch">
+            {this.state.data.map(e => (
+              <div className="col-12 col-md-6 col-lg-4">
+                <News
+                  key={e.articleLink}
+                  title={e.title}
+                  imgSrc={e.imgSrc}
+                  description={e.description}
+                  articleLink={e.articleLink}
+                />
+              </div>
+            ))}
           </div>
         ) : (
           <h1>Data is loading...</h1>
         )}
-      </div>
+      </>
     );
   }
 }
