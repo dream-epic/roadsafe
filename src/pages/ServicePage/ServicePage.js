160db0e2e3f8f702cb605eccc9403064d64ffb5a
heguanelvis <heguanelvis@gmail.com>
2019-06-11 19:04:57 -0500

    restyled service page

--- src/pages/ServicePage/ServicePage.js
+++ src/pages/ServicePage/ServicePage.js
@@ -6,22 +6,25 @@ import "./ServicePage.scss";
 export default class ServicePage extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row align-items-center d-flex-wrap justify-content-around">
-          <div className="col-12 col-lg-7 d-flex justify-content-center">
-            <Aside />
-          </div>
-          <div className="col-12 col-lg-5 d-flex justify-content-center">
-            <ServiceBot />
+      <>
+        <div className="container-fluid">
+          <div className="row align-items-center d-flex-wrap justify-content-around">
+            <div className="col-12 col-lg-7 d-flex justify-content-center">
+              <Aside />
+            </div>
+            <div className="col-12 col-lg-4 d-flex justify-content-center service-bot-wrapper">
+              <ServiceBot />
+            </div>
           </div>
         </div>
         <div className="imbed-map d-flex justify-content-center">
           <iframe
+            title="city-potholes-patched"
             src="https://data.cityofchicago.org/dataset/Potholes-Patched-Previous-Seven-Days/caad-5j9e/embed?width=800&height=600"
             className="patched-iframe"
           />
         </div>
-      </div>
+      </>
     );
   }
 }
